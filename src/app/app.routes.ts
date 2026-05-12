cb0574533d5a3b5cb619a573650cc8c2c292b20b
lnbernal <luis.nicolas.bernal@gmail.com>
2026-05-11 22:47:41 -0500

    Se suben archivos necesarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { ReportForm } from './pages/report-form/report-form';
+import { ReportList } from './pages/report-list/report-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: Home
+    },
+
+    {
+        path: 'login',
+        component: Login
+    },
+
+    {
+        path: 'register',
+        component: Register
+    },
+
+    {
+        path: 'dashboard',
+        component: Dashboard
+    },
+
+    {
+        path: 'nuevo-reporte',
+        component: ReportForm
+    },
+
+    {
+        path: 'mis-reportes',
+        component: ReportList
+    }
+
+];
